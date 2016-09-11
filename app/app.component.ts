9ce638db3b6d06ee85662fbf417e53dc797099b2
Amruth Pillai <im.amruth@gmail.com>
2016-09-12 00:09:22 +0530

    final push, updated starter template

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <h1>This is an Angular 2 App!</h1>
+    `
+})
+
+export class AppComponent {}
